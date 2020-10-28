d3c55815340a26ca0161f88c3e576b47dbe5b7f7
Erwin0109 <71189288+Erwin0109@users.noreply.github.com>
2020-10-28 17:47:44 +0700

    Initial commit

---
+++ system/resources/views/beranda.blade.php
@@ -0,0 +1,11 @@
+@extends('template.base')
+
+@section('content')
+	<div class="jumbotron">
+	  <h1 class="display-4">Selamat Datang Di MyShop!</h1>
+	  <p class="lead">Di MyShop ini terdapat banyak barang-barang elektronik seperti Hp, Laptop, dan TV yang memiliki harga murah dan bergaransi resmi.</p>
+	  <hr class="my-4">
+	  <p>MyShop ini telah mendapatkan sertifikat resmi E-Comerce.</p>
+	  <a class="btn btn-dark btn-lg" href="#" role="button">Learn more</a>
+	</div>
+@endsection
